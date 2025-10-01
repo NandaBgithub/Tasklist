11a2c36d6c60808e9a1901a52f629d799b910e7a
NandaBgithub <nandaputub@gmail.com>
2025-10-01 10:17:16 +1000

    fixed function reference

--- src/index.ts
+++ src/index.ts
@@ -14,9 +14,12 @@ let counterComponent:Counter = new Counter(body, taskState);
 let taskAdderComponent = new TaskAdder(body, taskManager, taskState);
 let taskDisplayComponent = new TaskDisplay(body, taskState);
 
-taskManager.subscribe('taskadded', taskDisplayComponent.update.bind(taskDisplayComponent));
-taskManager.subscribe('taskremoved', taskDisplayComponent.update.bind(taskDisplayComponent));
-taskManager.subscribe('taskremoved', counterComponent.update.bind(counterComponent));
+let unsub_taskadded_taskDispalyComponent: () => void = taskManager.subscribe('taskadded', 
+taskDisplayComponent.update.bind(taskDisplayComponent));
+let unsub_taskremoved_taskDisplayComponent: () => void = taskManager.subscribe('taskremoved', 
+taskDisplayComponent.update.bind(taskDisplayComponent));
+let unsub_taskremoved_counterComponent: ()=> void = taskManager.subscribe('taskremoved', 
+counterComponent.update.bind(counterComponent));
 
 taskAdderComponent.render();
 taskDisplayComponent.render();
